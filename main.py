5c70dead512267b9f3f29783c7fe4cf3e90ed3ec
maxi1805 <maxi1805@Maxi.localdomain>
2024-11-07 14:18:59 -0300

    fix: cors origins

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from src.search import get_llm_response
 from fastapi.middleware.cors import CORSMiddleware
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",
+    "http://localhost:5173/",
 ]
 
 app.add_middleware(
@@ -30,4 +30,23 @@ async def root():
 @app.post("/search", response_model=LLMResponse)
 async def search_endpoint(request: SearchRequest):
     response = get_llm_response(request.query)
+    
+    if "error" in response:
+        error_message = response["error"]
+        
+        if "timed out" in error_message:
+            raise HTTPException(status_code=504, detail="Gateway Timeout: The request to the LLM API timed out.")
+        
+        elif "HTTP error occurred" in error_message:
+            raise HTTPException(status_code=502, detail=error_message)
+        
+        elif "An error occurred while connecting" in error_message:
+            raise HTTPException(status_code=503, detail="Service Unavailable: Error connecting to the LLM API.")
+        
+        elif "Invalid JSON response" in error_message:
+            raise HTTPException(status_code=502, detail="Bad Gateway: Invalid JSON response from the LLM API.")
+        
+        else:
+            raise HTTPException(status_code=500, detail="Internal Server Error: An unexpected error occurred.")
+    
     return {"response": response}
